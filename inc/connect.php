3763255314d09fc4bf4e4bc2726aa37b23c0c492
Junyi <yunyi.xie@outlook.com>
2021-04-26 13:52:06 +0200

    Forgot exclamation mark

--- inc/connect.php
+++ inc/connect.php
@@ -13,7 +13,7 @@
     define('DIR', $_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI']);
 
 
-    if (file_exists(DIR.'inc/autoloader.php')) {
+    if (!file_exists(DIR.'inc/autoloader.php')) {
         exit('<h1>Could not include autoloader.php... The file is missing...</h1><p>Click <a href="https://github.com/junyi-xie/philomena/blob/main/inc/autoloader.php" target="_blank">here</a> to get the file...</p>');
     } else {
         include_once("autoloader.php");
