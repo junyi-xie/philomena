adde0a1475e73a351c43aad43a805236aa8e0874
Junyi <yunyi.xie@outlook.com>
2021-06-02 14:33:15 +0200

    Template for Signup.

--- signup.php
+++ signup.php
@@ -3,62 +3,98 @@
 
     use Philomena\Users;
 
+
     require_once 'config.php';
 
     $Users = new Users();
 
+    if ( isset($_POST) && !empty($_POST) ) 
+    {
+        $Users->SignUp($_POST);    
+    }
 
 
     include_once( INC . '/header.php');
 
-
-    echo '<div class="container" id="content" role="main">
+    echo '<div id="authentication">
     
-        <div class="row no-gutter">
+        <div class="container-fluid">
 
-                        
-            <div class="col-6">
+            <div class="row no-gutters" style="height: 100%;">
+        
+                <div class="col-lg-6 display-desktop" id="authentication__side_banner"></div>
+        
+                <div class="col-12 col-lg-6" id="authentication__signup">
+                            
+                    <section class="authentication_section__signup">
+
+                        <div class="authentication__wrapper">';
+
+                            flashMessage('signup');
+
+                            echo '<div class="authentication__heading">
+                                    
+                                <h2 class="authentication__title">Signup to Philomena</h2>
+
+                            </div>
+
+                            <form action="signup.php" accept-charset="UTF-8" method="POST" id="auth__signup">
+
+                            <label class="label">Full Name</label>
+
+                            <div class="row">
+
+                                <div class="col-12 col-lg-6">
+                                
+                                    <input class="form__text_field" type="text" name="first_name" placeholder="John" id="auth_first_name" required>
+                                
+                                </div>
+
+                                <div class="col-12 col-lg-6">
+                                
+                                    <input class="form__text_field" type="text" name="last_name" placeholder="Doe" id="auth_last_name" required>
+
+                                </div>
+
+                            </div>
+
+                            <label class="label">Email</label>
+
+                            <input class="form__text_field" type="email" name="email" placeholder="you@example.com" id="auth_email" required>
+
+                            <label class="label">Password</label>
+
+                            <input class="form__text_field" type="password" name="password" min="6" placeholder="Password" id="auth_password" required>
+
+                            <input class="form__text_field" type="password" name="confirm" min="6" placeholder="Confirm Password" id="auth_password_confirm" required>
+
+                            <div class="authentication__privacy_message">
+                
+                                <p>By creating your account, you agree to our <a href="#" target="_blank">Terms of Service</a> and <a href="#" target="_blank">Privacy Policy</a>.</p>
             
-            <form action="signup.php" method="POST">
+                            </div>
 
-        <input type="text" name="first_name" id="" placeholder="John">
-        <input type="text" name="last_name" id="" placeholder="Doe">
-        <input type="email" name="email" id="" placeholder="you@example.com">
-        <input type="password" name="password" id="" min="6" placeholder="Password">
-        <input type="password" name="confirm" id="" min="6" placeholder="Confirm Password">
-        <input type="submit" value="submit">
+                            <input class="button button__auth_login" type="submit" value="Sign Up" id="auth_signup">
 
-    </form>';
-            echo '</div>';
+                            </form>
 
-            echo '<!-- <div class="col-6" style="background-image: url("https://res.cloudinary.com/mhmd/image/upload/v1555917661/art-colorful-contemporary-2047905_dxtao7.jpg")"> -->
-';            
+                            <div class="authentication__login">
 
-                echo '<!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perferendis ab eos nesciunt modi, accusantium et dicta similique quam repellendus fugiat doloribus voluptatum suscipit quos unde tempora molestiae facere earum?</p> -->
-            <!-- </div> -->
+                                <p>Already have an account? <a href="login.php">Log In</a></p>
+
+                            </div>
+        
+                        </div>
+        
+                    </section>
+        
+                </div>
+
+            </div>
         
         </div>
-    
+
     </div>';
 
-
-// printr($_SESSION);
-if (isset($_POST) && !empty($_POST)) {
-    
-    $test = $Users->SignUp($_POST);
-
-
-    echo flashMessage('signup');
-}
-
-
-
-
-
-
     include_once( INC . '/footer.php');        
-    
-
-    
-
 ?>
